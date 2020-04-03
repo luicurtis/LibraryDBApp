93963301a2aadfe3ab9ccaca1df6b5b458ac41a6
Soroush <slighvan@sfu.ca>
2020-04-03 14:30:00 -0700

    Good Database, worked on eventActions and setting up the main.

--- src/eventActions.py
+++ src/eventActions.py
@@ -1,4 +1,23 @@
 # 5.Find Event, 6. Register for an Event
-
 import sqlite3
 conn = sqlite3.connect('library.db')
+
+def FindanEvent():
+    UserEventName = input("Enter the Event's name (If you would like to go back Enter 'X'): ")
+
+    if (UserEventName = 'X')
+        
+
+    with conn:
+        cur = conn.cursor()
+        myQuery = "SELECT * FROM LibraryEvents WHERE EventName=:Blank"
+
+        cur.execute(myQuery,{"Blank" : UserEventName})
+
+        row = cur.fetchone()
+        if row:
+            print("We do have the following Event occuring called, " + UserEventName + ": ")
+        else:
+            print("Unfortunately, we do not have any Events called " + UserEventName + "!\n")
+
+        print(row)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+import sys
 import sqlite3
 conn = sqlite3.connect('library.db')
-from src.itemActions import searchByTitle, 
-from src.eventActions import 
-from src.libraryActions import 
+#from src.itemActions import searchByTitle,
+from eventActions import FindanEvent
+#from src.libraryActions import
 
-print("Hello welcome to the library!!!!!!")
-choice = input("Choose from the following: \n 1. Search \n 2. Borrow \n 3. Return \n 4. Donate \n 5.Find Event \n 6. ... ")
+print("Hello, Welcome to the library!\n Please Select one of the following options by typing the associated number.\n")
+print("Choose from the following: \n 1. Search \n 2. Borrow \n 3. Return \n 4. Donate an item \n 5. Find an Event \n 6. Register for an Event \n 7. Volunteer \n 8. Ask for help\n 9. Exit\n")
 
-searchByTitle()
+choice = input("Please enter your decision: ")
+done = '0'
+while (done != '1'):
+
+    if (choice == '9'):
+        done = '1'
+
+    elif (choice == '5'):
+        FindanEvent()
+
+##Loop has ended
+print("Thank you for coming! See you soon.")
