c04b284a72c12cd615abff8a3ee5d7f17c37b89d
luicurtis <lui_curtis@hotmail.com>
2020-04-03 15:00:46 -0700

    Concatenation for search query

--- src/itemActions.py
+++ src/itemActions.py
@@ -4,24 +4,42 @@ conn = sqlite3.connect('library.db')
 # Search for a book in the library
 def searchBook(ISBN, title, genre, publisher):
     with conn:
+        queryDict = {}
+        queryDict["type"] = "Book"
+
+        myQuery = "SELECT * FROM Publications WHERE Type=:type"
+        # Concatenate the query based on what info we are given
+        if (ISBN != None):
+            myQuery += " AND ISBN=:ISBN"
+            queryDict["ISBN"] = ISBN
+        if (title != None):
+            myQuery += " AND Title=:title"
+            queryDict["title"] = title
+        if (genre != None):
+            myQuery += " AND Genre=:genre"
+            queryDict["genre"] = genre
+        if (publisher != None):
+            myQuery += " AND Publisher=:publisher"
+            queryDict["publisher"] = publisher
 
         cur = conn.cursor()
 
-        myQuery = "SELECT * FROM albums NATURAL JOIN artists WHERE Name=:myArtist"
-
-        cur.execute(myQuery,{"myArtist":myFavArtist})
+        cur.execute(myQuery,queryDict)
 
         #rows=cur.fetchall()
         rows = cur.fetchone()
         if rows:
-            print("We do have the following albums from your favorite artist, " + myFavArtist + ": ")
+            print("Here are the following search results: ")
+            for row in rows:
+                print(row[1])
+            print("\n")
         else:
-            print("Unfortunately, we do not have any albums by " + myFavArtist + "!\n")
-
-        for row in rows:
-            print(row[1])
-        print("\n")
-
+            print("Unfortunately, we do not have any books that fill those search requirements. Please try again.\n")
+        
+        # Not sure if this is needed
+        # it might close the connection in the main file?
+        if conn:
+            conn.close()
     return 
 
 def searchMagazine(ISBN, title, genre, publisher, contentRating):
---
+++ src/test_itemActions.py
@@ -0,0 +1,3 @@
+import sqlite3
+conn = sqlite3.connect('library.db')
+from src.itemActions import searchBook 
