a9717c96050433599577663a4414df4ebdbfc019
luicurtis <lui_curtis@hotmail.com>
2020-04-03 10:22:25 -0700

    Created function headers

--- src/itemActions.py
+++ src/itemActions.py
@@ -1,26 +1,55 @@
-#1. Search \n 2. Borrow \n 3. Return 
-
 import sqlite3
 conn = sqlite3.connect('library.db')
 
-def searchByTitle (title):
+# Search for a book in the library
+def searchBook(ISBN, title, genre, publisher):
     with conn:
 
-    cur = conn.cursor()
+        cur = conn.cursor()
 
-    myQuery = "SELECT * FROM albums NATURAL JOIN artists WHERE Name=:myArtist"
+        myQuery = "SELECT * FROM albums NATURAL JOIN artists WHERE Name=:myArtist"
 
-    cur.execute(myQuery,{"myArtist":myFavArtist})
+        cur.execute(myQuery,{"myArtist":myFavArtist})
 
-    #rows=cur.fetchall()
-    rows = cur.fetchone()
-    if rows:
-        print("We do have the following albums from your favorite artist, " + myFavArtist + ": ")
-    else:
-        print("Unfortunately, we do not have any albums by " + myFavArtist + "!\n")
+        #rows=cur.fetchall()
+        rows = cur.fetchone()
+        if rows:
+            print("We do have the following albums from your favorite artist, " + myFavArtist + ": ")
+        else:
+            print("Unfortunately, we do not have any albums by " + myFavArtist + "!\n")
 
-    for row in rows:
-        print(row[1])
-    print("\n")
+        for row in rows:
+            print(row[1])
+        print("\n")
 
     return 
+
+def searchMagazine(ISBN, title, genre, publisher, contentRating):
+
+    return
+
+def searchDVD(title, yearReleased, genre):
+
+    return
+
+def searchCD(title, genre, artist):
+
+    return
+
+def searchRecord(title, genre, artist):
+
+    return
+
+def searchVideogames(title, yearReleased, genre):
+
+    return
+
+def borrowItem(assetTag, cardNumber):
+
+    return
+
+def returnItem(assetTag, cardNumber):
+
+    return
+
+
